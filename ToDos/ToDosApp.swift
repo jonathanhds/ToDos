fd09d22e289986fa3e1b7bfb372c624bc27eb701
Jonathan Henrique de Souza <jonathan.henrique@icloud.com>
2022-12-06 14:11:47 -0300

    Initial Commit

---
+++ ToDos/ToDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDosApp.swift
+//  ToDos
+//
+//  Created by Jonathan Souza on 06/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
